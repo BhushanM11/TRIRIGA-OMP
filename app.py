84e3efc64ad4c82f9a1a6391f4e531862c01f7c0
Bhushan misal <Bhushan.misal@jll.com>
2024-06-20 21:09:38 +0530

    updated index.html file

--- app.py
+++ app.py
@@ -1,34 +1,3 @@
-# from flask import Flask, request
-# import requests
-# from requests.auth import HTTPBasicAuth
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return app.send_static_file('index.html')
-
-# @app.route('/create-package', methods=['POST'])
-# def create_package():
-#     # Get data from request
-#     data = request.get_json()
-#     username = data.get('username')
-#     package_name = data.get('packageName')
-#     password = "1Password*"  # This should be hidden in production level applications
-#     environment = "https://microsoft-dev.iwmsapp.com/tririga"
-#     package_description = "test-pipeline-omp-description"    
-    
-#     # Create request URL
-#     url = f'{environment}/api/p/v1/om/createEmptyPackage?packageName={package_name}&packageDescription={package_description}'
-
-#     # Make the GET request with basic auth
-#     response = requests.get(url, auth=HTTPBasicAuth(username, password))
-#     return response.json()
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
 from flask import Flask, request
 from flask_cors import CORS
 import requests
@@ -40,24 +9,6 @@ CORS(app)
 @app.route('/')
 def index():
  return app.send_static_file('index.html')
-
-# @app.route('/create-package', methods=['POST'])
-# def create_package():
-#     # Get data from request
-#     data = request.get_json()
-#     username = data.get('username')
-#     package_name = data.get('packageName')
-#     password = "1Password*"  # This should be hidden in production level applications
-#     environment = "https://microsoft-dev.iwmsapp.com/tririga"
-#     package_description = "test-pipeline-omp-description"    
-    
-#     # Create request URL
-#     url = f'{environment}/api/p/v1/om/createEmptyPackage?packageName={package_name}&packageDescription={package_description}'
-
-#     # Make the GET request with basic auth
-#     response = requests.get(url, auth=HTTPBasicAuth(username, password))
-#     return response.json()
-
 
 
 @app.route('/create-package', methods=['POST'])
